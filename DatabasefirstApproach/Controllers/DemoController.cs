0223f93d735f1ca9aa2054b11e1bae66eda3c316
Ashutosh Sahu <92512114+Ashutosh50@users.noreply.github.com>
2024-11-16 01:34:11 +0530

    DB1st

---
+++ DatabasefirstApproach/Controllers/DemoController.cs
@@ -0,0 +1,24 @@
+﻿using DatabasefirstApproach.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DatabasefirstApproach.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public readonly DemoEntities _demo;
+        public DemoController() 
+        {
+         _demo=new DemoEntities();
+        }
+        public ActionResult Getall()
+        {
+            var db = _demo.Stables.ToList();
+            return View(db);
+        }
+    }
+}
---
+++ DatabasefirstApproach/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DatabasefirstApproach.MvcApplication" Language="C#" %>
